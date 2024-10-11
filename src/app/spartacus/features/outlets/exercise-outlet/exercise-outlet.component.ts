fd0088dd1b601f1bf77ea124f74b124ec285168b
Mario Rojas <mrojas.leyva@gmail.com>
2024-10-11 02:56:29 -0600

    Exercise 3.4: Display Customized PDP Based on the Brand

--- src/app/spartacus/features/outlets/exercise-outlet/exercise-outlet.component.ts
+++ src/app/spartacus/features/outlets/exercise-outlet/exercise-outlet.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { OutletPosition } from '@spartacus/storefront';
+import { CurrentProductService } from '@spartacus/storefront';
+import { isNotNullable, Product } from '@spartacus/core';
+import { Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-exercise-outlet',
@@ -8,4 +12,19 @@ import { OutletPosition } from '@spartacus/storefront';
 })
 export class ExerciseOutletComponent {
   before: OutletPosition = OutletPosition.BEFORE;
+  
+  constructor(private currentProductService: CurrentProductService) {}
+
+  private product$: Observable<Product> = this.currentProductService.getProduct().pipe(filter(isNotNullable));
+
+  isSelectedBrand$: Observable<boolean> = this.hasBrand('canon');
+  
+  hasBrand(brand: string) : Observable<boolean> {
+    return this.product$.pipe(
+      map(
+        p =>
+          !!p.categories?.find(c => c.name?.toLowerCase() === brand.toLowerCase())
+      )
+    );
+  }
 }
