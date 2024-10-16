1c47254950cb97d7fe9ed33c105950b5065ea4d2
Mario Rojas <mrojas.leyva@gmail.com>
2024-10-15 18:31:14 -0600

    Exercise 8.2: Customize the Standard Product Details Page Routes

--- src/app/spartacus/features/custom-routing/custom-routing.module.ts
+++ src/app/spartacus/features/custom-routing/custom-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StaticPageComponent } from './static-page/static-page.component';
-import { UrlModule } from '@spartacus/core';
+import { OccConfig, PRODUCT_NORMALIZER, provideConfig, RoutingConfig, UrlModule } from '@spartacus/core';
 import { RouterModule } from '@angular/router';
 import { staticRoutes } from './static-page/custom-static-routes';
+import { ProductPrettyNameNormalizer } from './custom-routes/product-name.normalizer';
 
 
 
@@ -19,6 +20,40 @@ import { staticRoutes } from './static-page/custom-static-routes';
 
     // Custom Composable Storefront static routes
     RouterModule.forChild(staticRoutes),
-  ]
+  ],
+  providers: [
+    // Configure product routes
+    provideConfig(<RoutingConfig>{
+      routing: {
+        routes: {
+          product: {
+            paths: [
+              'product/:manufacturer/:productCode/:prettyName',
+              'product/:productCode/:name',
+            ],
+          },
+        },
+      },
+    }),
+
+    // Configure OCC product endpoint to get the manufacturer attribute in the response
+    provideConfig(<OccConfig>{
+      backend: {
+        occ: {
+          endpoints: {
+            productSearch: 'products/search?fields=products(code,manufacturer,name,summary,price(FULL),images(DEFAULT),stock(FULL),averageRating),facets,breadcrumbs,pagination(DEFAULT),sorts(DEFAULT),freeTextSearch&query=${query}',
+          },
+        },
+      },
+    }),
+
+    // Configure pretty name normalizer
+    {
+      provide: PRODUCT_NORMALIZER,
+      useExisting: ProductPrettyNameNormalizer,
+      multi: true,
+    }
+  ],
+  exports: [RouterModule]
 })
 export class CustomRoutingModule { }
