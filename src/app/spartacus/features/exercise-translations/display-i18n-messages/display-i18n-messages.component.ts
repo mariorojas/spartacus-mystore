3b0f02260bed125f15070baf8095ba6b39b5153d
Mario Rojas <mrojas.leyva@gmail.com>
2024-10-19 13:36:19 -0600

    Exercise 9.3b: Using the Translation Service

--- src/app/spartacus/features/exercise-translations/display-i18n-messages/display-i18n-messages.component.ts
+++ src/app/spartacus/features/exercise-translations/display-i18n-messages/display-i18n-messages.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { ActiveCartService } from '@spartacus/cart/base/core';
 import { Cart } from '@spartacus/cart/base/root';
-import { Observable } from 'rxjs';
+import { TranslationService } from '@spartacus/core';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-display-i18n-messages',
@@ -12,9 +13,15 @@ export class DisplayI18nMessagesComponent {
 
   cart$: Observable<Cart>;
   today: Number;
+  private deliveryItemsQuantity: number | undefined = 0;
+  componentMessage$: Observable<string> = of("");
 
-  constructor(protected activeCartService: ActiveCartService) {
+  constructor(protected activeCartService: ActiveCartService, private translationService: TranslationService) {
     this.cart$ = this.activeCartService.getActive();
     this.today = Date.now();
+    this.cart$.subscribe(cart => {
+      this.deliveryItemsQuantity = cart.deliveryItemsQuantity;
+      this.componentMessage$ = this.translationService.translate('miniCart.item', { count: this.deliveryItemsQuantity });
+    });
   }
 }
