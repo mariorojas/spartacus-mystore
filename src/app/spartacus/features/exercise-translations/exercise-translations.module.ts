6c5e87aa6c0a7b09410fed314eb911669f47ef49
Mario Rojas <mrojas.leyva@gmail.com>
2024-10-21 16:09:41 -0600

    Exercise 9.4: Lazy Loading of Translation Chunks and Global Messaging

--- src/app/spartacus/features/exercise-translations/exercise-translations.module.ts
+++ src/app/spartacus/features/exercise-translations/exercise-translations.module.ts
@@ -1,11 +1,26 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DisplayI18nMessagesComponent } from './display-i18n-messages/display-i18n-messages.component';
-import { I18nModule } from '@spartacus/core';
+import { GlobalMessageService, GlobalMessageType, I18nConfig, I18nModule, provideConfig, TranslationService } from '@spartacus/core';
 import { IconModule, OutletRefModule } from '@spartacus/storefront';
 import { CartBaseModule } from '@spartacus/cart/base';
+import { filter } from 'rxjs';
 
 
+const messageType = GlobalMessageType.MSG_TYPE_INFO;
+
+export function i18MessageFactory(
+  translationService: TranslationService,
+  messageService: GlobalMessageService
+) {
+  return (): Promise<any> => {
+    translationService.translate('customFeature.example', { param1: 5 , param2: 17})
+      .pipe(filter(i => !i.startsWith('[')))
+      // continue with the regular subscribe to the String observabler
+      .subscribe(i => messageService.add(`Custom label added: ${i}`, messageType));
+    return Promise.resolve();
+  }
+}
 
 @NgModule({
   declarations: [
@@ -17,6 +32,24 @@ import { CartBaseModule } from '@spartacus/cart/base';
     IconModule,
     OutletRefModule,
     CartBaseModule,
+  ],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: i18MessageFactory,
+      deps: [ TranslationService, GlobalMessageService ],
+      multi: true,
+    },
+    provideConfig(<I18nConfig>{
+      i18n: {
+        backend: {
+          loadPath: 'assets/i18n-assets/{{lng}}/{{ns}}.json',
+        },
+        chunks: {
+          custom: ['customFeature'],
+        },
+      },
+    }),
   ],
   exports: [DisplayI18nMessagesComponent],
 })
